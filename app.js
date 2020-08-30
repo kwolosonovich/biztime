5a6234df0ddd43b657f4a2f566db3dce99a12f2c
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-08-30 16:18:51 -0700

    added invoices route

--- app.js
+++ app.js
@@ -3,19 +3,14 @@
 const express = require("express");
 const app = express();
 const companiesRoutes = require("./routes/companies");
+const invoicesRoutes = require("./routes/invoices")
 const ExpressError = require("./expressError")
-// const db = require('./db')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/companies", companiesRoutes);
+app.use("/invoices", invoicesRoutes)
 
-// app.get('/', async (req, res, next) => {
-//   console.log('here')
-//   const data = await db.query(
-//     `SELECT * FROM companies`)
-//   return res.json(data.rows)
-// })
 
 /** 404 handler */
 
