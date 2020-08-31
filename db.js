8d410091339ab92426488ce4d76135622a394c10
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-08-31 13:52:27 -0700

    configed pg env vars; updated client

--- db.js
+++ db.js
@@ -1,13 +1,28 @@
 /** Database setup for BizTime. */
 const { Client } = require("pg")
-const secure_DB_URI = require("./secure")
+// const secure_DB_URI = require("./secure")
 
 // let DB_URI = secure_DB_URI;
 
+// let db = new Client({
+//   connectionString: secure_DB_URI
+// });
+
+
+let data;
+
+if (process.env.NODE_ENV === "test") {
+  data = "biztime_test";
+} else {
+  data = "biztime";
+}
+
 let db = new Client({
-  connectionString: secure_DB_URI
+  database: data
 });
 
 db.connect();
 
 module.exports = db;
+
+
--- secure.js
+++
@@ -1,5 +0,0 @@
-// secure DB_URI
-
-const secure_DB_URI = /* add DB_URI */
-
-module.exports = secure_DB_URI;
